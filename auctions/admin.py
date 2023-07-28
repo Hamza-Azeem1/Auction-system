cc3fcf77ac227ddc51357ce5061990b8cf17a8bf
ibnabii <noone@nowhere.net>
2023-07-28 12:50:55 +0200

    changed app config regarding database and user_auth for easier testing and timezone!

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Listing, Bid, Comment, User
+from .models import Listing, Bid, Comment
 
 # Register your models here.
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(Listing)
 admin.site.register(Bid)
 admin.site.register(Comment)
